64762a7c83e6a756f156c08dc7ea087d52caddec
Mirza <mirza.ismayilov@bil.omu.edu.tr>
2023-12-11 01:45:24 +0300

    Not Ortalamasi Hesaplayan Program
    if else yerine, ternary operatoru kullanmisimdir.

--- src/Main.java
+++
@@ -1,5 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Patika.dev FullStack Java Spring boot & React Bootcamp");
-    }
-}
---
+++ src/NotOrtalamasiHesaplayanProgram.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class NotOrtalamasiHesaplayanProgram {
+    public static void main(String[] args) {
+        int matematik, fizik, kimya, turkce, tarih, muzik;
+        double ortalama;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Matematik notunuzu girin: ");
+        matematik=sc.nextInt();
+        System.out.println("Fizik notunuzu girin: ");
+        fizik=sc.nextInt();
+        System.out.println("Kimya notunuzu girin: ");
+        kimya=sc.nextInt();
+        System.out.println("Turkce notunuzu girin: ");
+        turkce=sc.nextInt();
+        System.out.println("Tarih notunuzu girin: ");
+        tarih=sc.nextInt();
+        System.out.println("Muzik notunuzu girin: ");
+        muzik=sc.nextInt();
+        ortalama=(matematik+fizik+kimya+turkce+tarih+muzik)/6;
+        System.out.println("Ortalamaniz: "+ortalama);
+
+
+
+        String durum= ortalama>60 ? "Sinifi Gecti":"Sinifi kaldi";
+        System.out.println(durum);
+
+
+
+
+    }
+}
