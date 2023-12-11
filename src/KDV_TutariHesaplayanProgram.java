686f8d5a849df56cee017223dff9899828dce180
Mirza <mirza.ismayilov@bil.omu.edu.tr>
2023-12-11 10:36:34 +0300

    KDV Tutarı Hesaplayan Program

---
+++ src/KDV_TutariHesaplayanProgram.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class KDV_TutariHesaplayanProgram {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float KDV_siz_fiyat, KDV_li_fiyat, KDV_tutari;
+
+        System.out.println("KDV-ni hesaplamak istediginiz para degerini girin: ");
+        KDV_siz_fiyat= sc.nextFloat();
+
+        KDV_tutari=(KDV_siz_fiyat*18f/100f);
+        KDV_li_fiyat=KDV_siz_fiyat+KDV_tutari;
+
+        System.out.println("Girilen para degeri 18% KDV orani ile hesaplanacak");
+        System.out.println("KDV'siz fiyat:"+ KDV_siz_fiyat);
+        System.out.println("KDV'li fiyat: "+KDV_li_fiyat);
+        System.out.println("KDV tutari: "+ KDV_tutari);
+
+
+
+
+
+    }
+}
